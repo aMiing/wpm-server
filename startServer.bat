db965fda04f0ce6bdb464d7c7bb66ed503528321
王超明 <wangchaoming@wangchaomingdeMacBook-Pro.local>
2021-06-10 11:00:43 +0800

    优化系统启动项

--- startServer.bat
+++ startServer.bat
@@ -1,19 +1,28 @@
+title 启动管理系统
 
 @echo off
 :: 获取管理员权限
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 
-TIMEOUT /T 1
-@echo begin stop mysql server ...
-net stop mysql57
- 
-TIMEOUT /T 2
+#TIMEOUT /T 1
+#@echo begin stop mysql server ...
+#net stop mysql57
+
 @echo begin start mysql server ...
- 
-TIMEOUT /T 2
 net start mysql57
- 
+
+TIMEOUT /T 1
+@echo off
+start cmd /k "pm2 stop all" 
+
 TIMEOUT /T 3
 @echo off
-start cmd /k "pm2 stop all"
 start cmd /k "pm2 start app.js"
+
+@echo 即将通过默认浏览器打开系统...
+TIMEOUT /T 3
+start http://localhost:3000/#/login
+
+TIMEOUT /T 1
+@echo off
+start cmd /k "taskkill /f /t /im cmd.exe"
